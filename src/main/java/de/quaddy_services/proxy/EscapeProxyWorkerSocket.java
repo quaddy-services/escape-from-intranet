f96a3cb2ff4d49381dab53aefb779e8f00d55050
Stefan <mail@quaddy-services.de>
2017-01-24 22:17:42 +0100

    forward all traffic

--- src/main/java/de/quaddy_services/proxy/EscapeProxyWorkerSocket.java
+++ src/main/java/de/quaddy_services/proxy/EscapeProxyWorkerSocket.java
@@ -5,17 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.lang.reflect.InvocationTargetException;
-import java.net.Authenticator;
-import java.net.InetSocketAddress;
-import java.net.PasswordAuthentication;
-import java.net.Proxy;
-import java.net.Proxy.Type;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
-import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,7 +39,7 @@ public class EscapeProxyWorkerSocket extends Thread {
 		LOGGER.debug("Started with {}", socket);
 		List<String> tempHeaders;
 		try {
-			tempHeaders = readHeader(socket);
+			tempHeaders = readHeaders(socket);
 		} catch (IOException e) {
 			LOGGER.error("Error", e);
 			try {
@@ -74,78 +67,68 @@ public class EscapeProxyWorkerSocket extends Thread {
 			}
 			return;
 		}
-		String tempFirstLine = tempHeaders.get(0);
-		StringTokenizer tempFirstLineTokens = new StringTokenizer(tempFirstLine, " ");
-		String tempFirstLine1 = tempFirstLineTokens.nextToken();
-		if (tempFirstLine1.equals("CONNECT")) {
-			// https
-			String tempHostAndPort = tempFirstLineTokens.nextToken();
-			String tempHttpVersion = tempFirstLineTokens.nextToken();
 
-			Socket tempTargetSocket;
-			try {
-				tempTargetSocket = new Socket(config.getProxyHost(), Integer.valueOf(config.getProxyPort()));
-				OutputStream tempProxyOut = tempTargetSocket.getOutputStream();
-				boolean tempProxyAuthAlreadySent = false;
-				for (String tempHeader : tempHeaders) {
-					if (tempHeader.toLowerCase().startsWith("proxy-authorization:")) {
-						LOGGER.debug("Take Proxy auth from original request {}", tempHeader);
-						tempProxyAuthAlreadySent = true;
-					}
-					LOGGER.debug("Forward {}", tempHeader);
-					tempProxyOut.write((tempHeader + "\r\n").getBytes());
+		Socket tempTargetSocket;
+		try {
+			tempTargetSocket = new Socket(config.getProxyHost(), Integer.valueOf(config.getProxyPort()));
+			OutputStream tempProxyOut = tempTargetSocket.getOutputStream();
+			boolean tempProxyAuthAlreadySent = false;
+			for (String tempHeader : tempHeaders) {
+				if (tempHeader.toLowerCase().startsWith("proxy-authorization:")) {
+					LOGGER.debug("Take Proxy auth from original request {}", tempHeader);
+					tempProxyAuthAlreadySent = true;
 				}
-				if (!tempProxyAuthAlreadySent) {
-					String tempAuth = new String(Base64.getEncoder().encode((config.getProxyUser() + ":" + config.getProxyPassword()).getBytes()));
-					String tempProxyHeaderAuth = "Proxy-Authorization: Basic " + tempAuth;
-					LOGGER.debug("Add {}", tempProxyHeaderAuth);
-					tempProxyOut.write((tempProxyHeaderAuth + "\r\n").getBytes());
-				}
-				tempProxyOut.write(("\r\n").getBytes());
-				tempProxyOut.flush();
-			} catch (IOException | RuntimeException e) {
-				LOGGER.error("Error connecting to " + tempHostAndPort + " via " + config.getProxyHost(), e);
-				try {
-					OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-					String tempMessage = e.getMessage();
-					// Maybe: Unable to tunnel through proxy. Proxy returns "HTTP/1.0 407 Proxy Authentication Required"
-					int tempHttpPos = tempMessage.indexOf("HTTP/");
-					if (tempHttpPos >= 0) {
-						// Reply with original Message
-						outputStreamWriter.write(tempMessage.substring(tempHttpPos) + "\r\n");
-					} else {
-						outputStreamWriter.write(tempHttpVersion + " 404 " + tempMessage + "\r\n");
-					}
-					outputStreamWriter.write("Proxy-agent: escape-from-intranet\r\n");
-					outputStreamWriter.write("\r\n");
-					outputStreamWriter.flush();
-				} catch (IOException e2) {
-					LOGGER.error("Error sending response to " + socket, e2);
-				}
-				return;
+				LOGGER.debug("Forward {}", tempHeader);
+				tempProxyOut.write((tempHeader + "\r\n").getBytes());
 			}
-
-			new Thread() {
-				@Override
-				public void run() {
-					forwardData(tempTargetSocket, socket);
+			if (!tempProxyAuthAlreadySent) {
+				String tempAuth = new String(Base64.getEncoder().encode((config.getProxyUser() + ":" + config.getProxyPassword()).getBytes()));
+				String tempProxyHeaderAuth = "Proxy-Authorization: Basic " + tempAuth;
+				LOGGER.debug("Add {}", tempProxyHeaderAuth);
+				tempProxyOut.write((tempProxyHeaderAuth + "\r\n").getBytes());
+			}
+			tempProxyOut.write(("\r\n").getBytes());
+			tempProxyOut.flush();
+		} catch (IOException | RuntimeException e) {
+			LOGGER.error("Error connecting to proxy via " + config.getProxyHost() + " " + config.getProxyPort(), e);
+			try {
+				OutputStreamWriter outputStreamWriter = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+				String tempMessage = e.getMessage();
+				// Maybe: Unable to tunnel through proxy. Proxy returns "HTTP/1.0 407 Proxy Authentication Required"
+				int tempHttpPos = tempMessage.indexOf("HTTP/");
+				if (tempHttpPos >= 0) {
+					// Reply with original Message
+					outputStreamWriter.write(tempMessage.substring(tempHttpPos) + "\r\n");
+				} else {
+					outputStreamWriter.write("HTTP/1.0" + " 404 " + tempMessage + "\r\n");
 				}
-			}.start();
-			new Thread() {
-				@Override
-				public void run() {
-					forwardData(socket, tempTargetSocket);
-				}
-			}.start();
-		} else {
-			// http
+				outputStreamWriter.write("Proxy-agent: escape-from-intranet\r\n");
+				outputStreamWriter.write("\r\n");
+				outputStreamWriter.flush();
+			} catch (IOException e2) {
+				LOGGER.error("Error sending response to " + socket, e2);
+			}
+			return;
 		}
+
+		new Thread() {
+			@Override
+			public void run() {
+				forwardData(tempTargetSocket, socket);
+			}
+		}.start();
+		new Thread() {
+			@Override
+			public void run() {
+				forwardData(socket, tempTargetSocket);
+			}
+		}.start();
 	}
 
 	/**
 	 *
 	 */
-	private List<String> readHeader(Socket aSocket) throws IOException {
+	private List<String> readHeaders(Socket aSocket) throws IOException {
 		List<String> tempLines = new ArrayList<String>();
 		InputStream tempInputStream = aSocket.getInputStream();
 		int tempByte = tempInputStream.read();
@@ -176,7 +159,11 @@ public class EscapeProxyWorkerSocket extends Thread {
 	/**
 	 * http://stackoverflow.com/questions/9357585/creating-a-java-proxy-server-that-accepts-https
 	 */
-	private static void forwardData(Socket inputSocket, Socket outputSocket) {
+	private void forwardData(Socket inputSocket, Socket outputSocket) {
+		Thread tempCurrentThread = Thread.currentThread();
+		String tempName = inputSocket.getInetAddress().getHostName() + ">" + outputSocket.getInetAddress().getHostName() + "-" + tempCurrentThread.getName();
+		tempCurrentThread.setName(tempName);
+		LOGGER.info("Start forwarding...");
 		try {
 			InputStream inputStream = inputSocket.getInputStream();
 			try {
@@ -188,8 +175,10 @@ public class EscapeProxyWorkerSocket extends Thread {
 						read = inputStream.read(buffer);
 						if (read > 0) {
 							if (LOGGER.isDebugEnabled()) {
-								LOGGER.debug("Write to " + outputSocket + ":");
-								LOGGER.debug(new String(buffer, 0, read));
+								LOGGER.debug("Write to " + outputSocket + ":" + read + " bytes");
+								if (LOGGER.isTraceEnabled()) {
+									LOGGER.trace(new String(buffer, 0, read));
+								}
 							}
 							outputStream.write(buffer, 0, read);
 							if (inputStream.available() < 1) {
@@ -208,7 +197,8 @@ public class EscapeProxyWorkerSocket extends Thread {
 				}
 			}
 		} catch (IOException e) {
-			LOGGER.error("Error", e);
+			LOGGER.info("Closed"+ e);
+			LOGGER.debug("Error", e);
 		}
 	}
 }
