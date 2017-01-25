e563b504bee31e926309bbeba6dc1373ec7b4555
Stefan <mail@quaddy-services.de>
2017-01-25 20:20:11 +0100

    support system tray

--- src/main/java/de/quaddy_services/proxy/EscapeProxy.java
+++ src/main/java/de/quaddy_services/proxy/EscapeProxy.java
@@ -1,22 +1,36 @@
 package de.quaddy_services.proxy;
 
+import java.awt.AWTException;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.Image;
 import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import javax.imageio.ImageIO;
+import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import de.quaddy_services.proxy.events.PortStatusListener;
 
 /**
  *
@@ -25,7 +39,7 @@ public class EscapeProxy {
 
 	private static Logger LOGGER;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		initializeLogger();
 
 		LOGGER.info("Start");
@@ -42,12 +56,35 @@ public class EscapeProxy {
 		// See java.awt.EventDispatchThread.handlerPropName
 		System.setProperty("sun.awt.exception.handler", LoggingUncaughtExceptionHandler.class.getName());
 
-		addExitListener(tempEscapeProxyFrame, tempProperties);
-		initSystemTray(tempEscapeProxyFrame);
+		trayImage = new Image[] { getImage("offline.png"), getImage("online.png") };
 
-		setVisible(tempEscapeProxyFrame);
+		addExitListener(tempEscapeProxyFrame, tempProperties);
+		initSystemTray(tempEscapeProxyFrame, tempEscapeProxyConfig);
+
+		setVisible(tempEscapeProxyFrame, tempEscapeProxyConfig);
+		addStatusListener(tempEscapeProxyFrame, tempEscapeProxyConfig);
 
 		new EscapeProxyWorkerAccept(tempEscapeProxyConfig).start();
+	}
+
+	/**
+	 *
+	 */
+	private static void addStatusListener(EscapeProxyFrame aEscapeProxyFrame, EscapeProxyConfig aEscapeProxyConfig) {
+
+		aEscapeProxyConfig.addStatusListener(new PortStatusListener() {
+
+			@Override
+			public void statusChanged(boolean aOkFlag, @SuppressWarnings("unused") String aText) {
+				EventQueue.invokeLater(new Runnable() {
+
+					@Override
+					public void run() {
+						aEscapeProxyFrame.setIconImage(trayImage[aOkFlag ? 1 : 0]);
+					}
+				});
+			}
+		});
 	}
 
 	/**
@@ -90,26 +127,83 @@ public class EscapeProxy {
 		return tempFile;
 	}
 
+	private static Image[] trayImage;
+
 	/**
+	 * @param aEscapeProxyConfig
 	 *
 	 */
-	private static void initSystemTray(EscapeProxyFrame aEscapeProxyFrame) {
+	private static void initSystemTray(EscapeProxyFrame aEscapeProxyFrame, EscapeProxyConfig aEscapeProxyConfig) {
 		if (SystemTray.isSupported()) {
-			// TODO
+			EventQueue.invokeLater(new Runnable() {
+				/**
+				 * Init gui in eventqueue
+				 */
+				@Override
+				public void run() {
+					try {
+						SystemTray tempSystemTray = SystemTray.getSystemTray();
+						TrayIcon tempTrayIcon = new TrayIcon(trayImage[0]);
+						tempTrayIcon.addMouseListener(new MouseAdapter() {
+							@Override
+							public void mouseClicked(@SuppressWarnings("unused") MouseEvent aE) {
+								aEscapeProxyFrame.setVisible(true);
+								aEscapeProxyFrame.toFront();
+								aEscapeProxyFrame.setState(Frame.NORMAL);
+							}
+						});
+						tempSystemTray.add(tempTrayIcon);
+					} catch (AWTException e) {
+						LOGGER.error("Error", e);
+					}
+				}
+			});
+			aEscapeProxyConfig.addStatusListener(new PortStatusListener() {
+
+				@Override
+				public void statusChanged(boolean aOkFlag, String aText) {
+					EventQueue.invokeLater(new Runnable() {
+
+						@Override
+						public void run() {
+							TrayIcon tempTrayIcon = SystemTray.getSystemTray().getTrayIcons()[0];
+							tempTrayIcon.setImageAutoSize(true);
+							tempTrayIcon.setImage(trayImage[aOkFlag ? 1 : 0]);
+							tempTrayIcon.setToolTip(aText);
+						}
+					});
+				}
+			});
 		}
 	}
 
 	/**
+	 * @throws IOException
 	 *
 	 */
-	private static void setVisible(final EscapeProxyFrame tempEscapeProxyFrame) {
+	private static Image getImage(String aString) throws IOException {
+		InputStream tempIn = EscapeProxy.class.getClassLoader().getResourceAsStream(aString);
+		BufferedImage tempImage = ImageIO.read(tempIn);
+		return tempImage;
+	}
+
+	/**
+	 * @param aEscapeProxyConfig
+	 *
+	 */
+	private static void setVisible(final EscapeProxyFrame tempEscapeProxyFrame, EscapeProxyConfig aEscapeProxyConfig) {
 		EventQueue.invokeLater(new Runnable() {
 			/**
 			 *
 			 */
 			@Override
 			public void run() {
+				tempEscapeProxyFrame.setIconImage(trayImage[0]);
 				tempEscapeProxyFrame.setVisible(true);
+				String tempProxyUser = aEscapeProxyConfig.getProxyUser();
+				if (tempProxyUser != null && tempProxyUser.length() > 0) {
+					tempEscapeProxyFrame.setState(Frame.ICONIFIED);
+				}
 			}
 		});
 	}
@@ -124,19 +218,42 @@ public class EscapeProxy {
 			 *
 			 */
 			@Override
-			public void windowClosing(WindowEvent aE) {
-				saveConfig(aProperties);
-				super.windowClosing(aE);
-				LOGGER.info("Exit");
-				System.exit(0);
+			public void windowClosing(@SuppressWarnings("unused") WindowEvent aE) {
+				tempEscapeProxyFrame.setVisible(false);
+				Timer tempTimer = new Timer(5000, new ActionListener() {
+
+					@Override
+					public void actionPerformed(@SuppressWarnings("unused") ActionEvent aE2) {
+						saveConfig(aProperties);
+						LOGGER.info("Exit");
+						System.exit(0);
+					}
+				});
+				tempTimer.start();
+				if (SystemTray.isSupported()) {
+					TrayIcon tempTrayIcon = SystemTray.getSystemTray().getTrayIcons()[0];
+					tempTrayIcon.displayMessage("Shutdown", "localhost proxy\nis shutting down...", MessageType.WARNING);
+				}
 			}
+
 			/**
-			 * 
+			 *
 			 */
 			@Override
 			public void windowDeactivated(WindowEvent aE) {
 				super.windowDeactivated(aE);
 				saveConfig(aProperties);
+			}
+
+			/**
+			 *
+			 */
+			@Override
+			public void windowIconified(WindowEvent aE) {
+				super.windowIconified(aE);
+				if (SystemTray.isSupported()) {
+					tempEscapeProxyFrame.setVisible(false);
+				}
 			}
 		});
 	}
