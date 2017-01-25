84d34365993192ab9fc7289efc9b77ce8b0f8a29
Stefan <mail@quaddy-services.de>
2017-01-25 21:26:05 +0100

    accept connections from localhost only.

--- src/main/java/de/quaddy_services/proxy/EscapeProxyWorkerAccept.java
+++ src/main/java/de/quaddy_services/proxy/EscapeProxyWorkerAccept.java
@@ -2,9 +2,13 @@ package de.quaddy_services.proxy;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -108,17 +112,66 @@ public class EscapeProxyWorkerAccept extends Thread {
 			try {
 				Socket tempSocket = aServerSocket.accept();
 
-				String tempMsg = "Got connection from " + tempSocket.getInetAddress();
+				InetAddress tempInetAddress = tempSocket.getInetAddress();
+				String tempMsg = "Got connection from " + tempInetAddress;
 				LOGGER.info(tempMsg);
 
-				EscapeProxyWorkerSocket tempEscapeProxyWorkerSocket = new EscapeProxyWorkerSocket(config, tempSocket);
-				tempEscapeProxyWorkerSocket.start();
+				if (isLocalhost(tempInetAddress)) {
+					EscapeProxyWorkerSocket tempEscapeProxyWorkerSocket = new EscapeProxyWorkerSocket(config, tempSocket);
+					tempEscapeProxyWorkerSocket.start();
+				} else {
+					config.fireLogEvent(tempInetAddress+" not localhost, closing " + tempSocket);
+					tempSocket.close();
+				}
 			} catch (IOException | RuntimeException e) {
 				LOGGER.error("Socket " + serverSocket + " finished " + e);
 				LOGGER.debug("Details", e);
 				return;
 			}
 		}
+	}
+
+	private Set<String> isLocalhostCache = new HashSet<>();
+	/**
+	 *
+	 */
+	private boolean isLocalhost(InetAddress aInetAddress) {
+		String tempHostAddress = aInetAddress.getHostAddress();
+		if (isLocalhostCache.contains(tempHostAddress)) {
+			return true;
+		}
+		boolean tempLocalhostDetect = isLocalhostDetect(aInetAddress);
+		if (tempLocalhostDetect) {
+			isLocalhostCache.add(tempHostAddress);
+		}
+		return tempLocalhostDetect;
+	}
+
+	/**
+	 *
+	 */
+	private boolean isLocalhostDetect(InetAddress aInetAddress) {
+		Enumeration<NetworkInterface> tempNetworkInterfaces;
+		try {
+			tempNetworkInterfaces = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException e) {
+			LOGGER.error("Error, assuming localhost", e);
+			return true;
+		}
+		if (tempNetworkInterfaces != null) {
+			while (tempNetworkInterfaces.hasMoreElements()) {
+				NetworkInterface tempNetworkInterface = tempNetworkInterfaces.nextElement();
+				Enumeration<InetAddress> tempInetAddresses = tempNetworkInterface.getInetAddresses();
+				while (tempInetAddresses.hasMoreElements()) {
+					InetAddress tempInetAddress = tempInetAddresses.nextElement();
+					if (tempInetAddress.equals(aInetAddress)) {
+						LOGGER.debug("Match {} on {}", tempInetAddress, tempNetworkInterface);
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	/**
