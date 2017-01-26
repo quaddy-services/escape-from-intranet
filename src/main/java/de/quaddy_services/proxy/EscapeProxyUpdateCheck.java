abff21adeecc9e138de17cfff003f407f2d24a65
Stefan CORDES <stefan.cordes@canda.com>
2017-01-26 21:06:03 +0100

    TODO EscapeProxyUpdateCheck #2

---
+++ src/main/java/de/quaddy_services/proxy/EscapeProxyUpdateCheck.java
@@ -0,0 +1,11 @@
+package de.quaddy_services.proxy;
+
+/**
+ * TODO
+ * 
+ * https://api.github.com/repos/quaddy-services/escape-from-intranet/releases/latest
+ * html_url": "https://github.com/quaddy-services/escape-from-intranet/releases/tag/v1.4",
+ */
+public class EscapeProxyUpdateCheck {
+
+}
