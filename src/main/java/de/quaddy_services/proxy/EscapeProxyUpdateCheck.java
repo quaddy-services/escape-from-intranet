1db258cd4dac8623f3220d2649b9dabe4aa588be
Stefan CORDES <stefan.cordes@canda.com>
2017-01-27 00:41:01 +0100

    prepare

--- src/main/java/de/quaddy_services/proxy/EscapeProxyUpdateCheck.java
+++ src/main/java/de/quaddy_services/proxy/EscapeProxyUpdateCheck.java
@@ -1,5 +1,14 @@
 package de.quaddy_services.proxy;
 
+import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import de.quaddy_services.proxy.logging.Logger;
+import de.quaddy_services.proxy.logging.LoggerFactory;
+
 /**
  * TODO
  * 
@@ -8,4 +17,45 @@ package de.quaddy_services.proxy;
  */
 public class EscapeProxyUpdateCheck {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(EscapeProxyUpdateCheck.class);
+
+	/**
+	 */
+	String updateUrl() {
+		String aJsonString = readReleases();
+		try {
+			Map tempJson = jsonToMap(aJsonString);
+			Object tempObject = tempJson.get("html_url");
+			return tempObject == null ? null : tempObject.toString();
+		} catch (ScriptException e) {
+			LOGGER.error("Error", e);
+			return null;
+		}
+
+	}
+
+	/**
+	 * 
+	 */
+	private String readReleases() {
+		return null;
+	}
+
+	/**
+	 * 
+	 * Thanks to http://www.adam-bien.com/roller/abien/entry/converting_json_to_map_with
+	 * @throws ScriptException 
+	 */
+	private Map jsonToMap(String aJsonString) throws ScriptException {
+		ScriptEngineManager sem = new ScriptEngineManager();
+		ScriptEngine engine = sem.getEngineByName("javascript");
+
+		String json = new String(aJsonString);
+		String script = "Java.asJSONCompatible(" + json + ")";
+		Object result = engine.eval(script);
+
+		Map contents = (Map) result;
+
+		return contents;
+	}
 }
