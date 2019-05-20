336aeffca632327d2b26d047e2a443cb7fa501c6
Stefan Cordes <2789123+quaddy-services@users.noreply.github.com>
2019-05-20 20:32:30 +0200

    use nsis-3.04-log.zip

--- build.bat
+++ build.bat
@@ -5,20 +5,21 @@ if %errorlevel% == 1 goto end
 echo on
 for /F "delims== tokens=1,2" %%F in (target\maven-archiver\pom.properties) do set %%F=%%G
 
-if x%NSIS_HOME%x == xx goto error_no_nsis
+if x%NSIS_HOME%x == xx Echo Could not generate Windows Setup as NSIS_HOME is not set.
+if x%NSIS_HOME%x == xx goto error_nsis
+
 
 %NSIS_HOME%\makensis /DdisplayVersion=%version% src/main/nsis/windows-setup.nsi
-if %errorlevel% == 1 pause
-if %errorlevel% == 1 goto end
+if %errorlevel% == 1 goto error_nsis
 
 Echo Finished.
 
 goto end
 
-:error_no_nsis
+:error_nsis
 echo off
-Echo Could not generate Windows Setup as NSIS_HOME is not set.
-Echo Download nsis-2.51.zip from https://sourceforge.net/projects/nsis/files/NSIS%202/2.51/
+Echo Error on NSIS. Maybe that helps:
+Echo Download nsis-3.04-log.zip from https://sourceforge.net/projects/nsis/files/NSIS%203/3.04/
 Echo Unzip it and refer it via NSIS_HOME
 goto end
 
