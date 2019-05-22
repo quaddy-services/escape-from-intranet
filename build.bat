060b46170a096cb875a50eaa72a3fb8cb91ab5c4
Stefan Cordes <stefan.cordes@canda.de>
2019-05-22 12:38:28 +0200

    Info about how to unblock the setup #8

--- build.bat
+++ build.bat
@@ -16,11 +16,19 @@ Echo Finished.
 
 goto end
 
+rem prepare for signing
+:error_sdk
+echo off
+echo install windows 10 SDK via https://visualstudio.microsoft.com/
+echo and component Windows 10 SDK
+echo and set WIN10_SDK_HOME=C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x86
+goto end
+
 :error_nsis
 echo off
 Echo Error on NSIS. Maybe that helps:
 Echo Download nsis-3.04-log.zip from https://sourceforge.net/projects/nsis/files/NSIS%203/3.04/
-Echo Unzip it and refer it via NSIS_HOME
+Echo Unzip it and refer it via set NSIS_HOME=C:\programs\nsis-3.04-log
 goto end
 
 
